25683c378baf7aae0ce68ad7958af8222d317efa
Your Name <perezfernando95@gmail.com>
2023-01-16 23:54:34 -0300

    Creating Cart

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const cardContainer = document.getElementById('card-container')
 const btnBackPage = document.getElementById('btn-back-page')
 const btnNextPage = document.getElementById('btn-next-page')
 const numPage = document.getElementById('num-page')
+const spinner = document.getElementById('spinner')
 const categories = document.querySelectorAll('.btn-category')
+const cart = document.querySelector('.cart-icon')
+
 
 const pagination = {
     start: 0,
@@ -10,6 +13,7 @@ const pagination = {
     categorie: 'best',
     lengthData: 0
 }
+
 
 // ----------------------------------Configuración de la API-------------------------------
 
@@ -26,31 +30,49 @@ const options = {
 // ---------------------------------------------------------------------------------------
 
 const fetching = async (urlFilter) => {
+    showSpinner(true)
     const res = await fetch(urlBase + urlFilter, options)
     const data = await res.json()
-    return data
+    return [data]
 }
 
 const renderCard = async (data, start, end) => {
+    showSpinner(false)
     const sliceArray = data.slice(start, end)
     const arrayCards = await sliceArray.map(card => {
         cardContainer.innerHTML +=
             `<div class="card">
-        <img src=${card.thumbnail} alt=${card.title} />
-        <h3>${card.title}</h3>
-        <p>${card.short_description}</p>
-        <div class="buy-container">
-            <h2 class="price"><span>U$D</span>${' ' + (card.id / 5).toFixed(2)}</h2>
-            <button class="btn-add-cart">
+                <img src=${card.thumbnail} alt=${card.title}/>
+                <h3>${card.title}</h3>
+                <p>${card.short_description}</p>
+                <div class="buy-container">
+                <h2 class="price"><span>U$D</span>${' ' + (card.id / 5).toFixed(2)}</h2>
+                <button class="btn-add-cart" data-id=${card.id}>
                 <i class="fa-sharp fa-solid fa-cart-plus add-cart"></i>
-            </button>
-        </div>`
-
+                </button>
+                </div>
+                </div>`
     })
 
     return arrayCards
 }
-
+const showSpinner = (condition) => {
+    if (condition) {
+        cardContainer.innerHTML = `<div class="sk-cube-grid" id="spinner">
+                                    <div class="sk-cube sk-cube1"></div>
+                                    <div class="sk-cube sk-cube2"></div>
+                                    <div class="sk-cube sk-cube3"></div>
+                                    <div class="sk-cube sk-cube4"></div>
+                                    <div class="sk-cube sk-cube5"></div>
+                                    <div class="sk-cube sk-cube6"></div>
+                                    <div class="sk-cube sk-cube7"></div>
+                                    <div class="sk-cube sk-cube8"></div>
+                                    <div class="sk-cube sk-cube9"></div>
+                                </div>`
+    } else {
+        cardContainer.innerHTML = ''
+    }
+}
 
 const backPage = () => {
     cardContainer.innerHTML = ''
@@ -61,7 +83,6 @@ const backPage = () => {
     pagination.start -= 20
     pagination.end -= 20
     btnNextPage.classList.remove('display-none')
-    // console.log(pagination.start, pagination.end)
 
     if (pagination.start < 11) {
         btnBackPage.className += ' display-none'
@@ -81,14 +102,12 @@ const nextPage = () => {
         btnNextPage.className += ' display-none'
     }
     btnBackPage.classList.remove('display-none')
-    console.log(pagination.start, pagination.end)
 
     cardContainer.innerHTML = ''
     numPage.innerHTML = ((pagination.start / 20) + 1)
     if (pagination.categorie == 'best') {
         showBest()
     } else showCategory(pagination.categorie)
-    console.log(pagination.lengthData)
 }
 
 const showBest = async () => {
@@ -104,7 +123,6 @@ const showCategory = async (categorie) => {
 
     const dataFetched = await fetching(`games?category=${categorie}`)
     pagination.lengthData = dataFetched.length
-    console.log(pagination.end, pagination.lengthData)
     if (pagination.end > pagination.lengthData) {
         btnNextPage.className += ' display-none'
     }
@@ -112,6 +130,7 @@ const showCategory = async (categorie) => {
 }
 
 const selectCategorie = async (e) => {
+
     pagination.start = 0
     pagination.end = 20
     numPage.innerHTML = ''
@@ -124,25 +143,34 @@ const selectCategorie = async (e) => {
     cardContainer.innerHTML = ''
     if (categorySelected == 'best') {
         showBest()
+    } else {
+        showCategory(categorySelected)
     }
-
     e.target.classList.add('selected')
-    showCategory(categorySelected)
 }
-// const arrayLoco = categories.forEach(cat => console.log(cat))
+
+const cartHandler = () => {
+    console.log("Abriendo carrito...")
+}
+
+const addToCart = () => {
+    console.log("Agregando al carrito...")
+}
 // -----------------------------------------------------------------------------------------------
 
-btnBackPage.addEventListener('click', backPage)
-btnNextPage.addEventListener('click', nextPage)
-categories.forEach(cat => { cat.addEventListener('click', selectCategorie) })
 
 
 const init = () => {
     showBest()
+    categories.forEach(cat => { cat.addEventListener('click', selectCategorie) })
+    btnBackPage.addEventListener('click', backPage)
+    btnNextPage.addEventListener('click', nextPage)
+    cart.addEventListener('click', cartHandler)
+
 }
 
-init()
-// document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", init);
+
 
 
 
